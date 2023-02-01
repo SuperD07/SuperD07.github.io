a2e2cc641d942552d8b9842d75a224ac975e41bc
SuperD07 <118451498+SuperD07@users.noreply.github.com>
2023-02-01 11:50:49 +0100

    save

--- Player.js
+++ Player.js
@@ -1,9 +1,14 @@
 class Player {
-    constructor () {
+    constructor ({ initialPosition, initialVelocity }) {
         this.factory = new PlayerStateFactory();
         this.groundedState = this.factory.groundedState;
         this.fallingState = this.factory.fallingState;
         this.jumpingState = this.factory.fallingState;
-        this.currentState;
+
+        this.currentState = this.groundedState;
+
+
+        this.position = initialPosition;
+        this.velocity = initialVelocity;
     }
 }
---
+++ utils.js
@@ -0,0 +1,12 @@
+class Vector {
+    constructor (x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+class Hitbox {
+    constructor (w, h) {
+        this.width = w;
+        this.height = h;
+    }
+}
