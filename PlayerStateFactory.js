6113a94ff9cd7ce15ce6d755133dc5942d4f5bac
SuperD07 <118451498+SuperD07@users.noreply.github.com>
2023-08-29 14:31:42 +0200

    Delete PlayerStateFactory.js

--- PlayerStateFactory.js
+++
@@ -1,7 +0,0 @@
-class PlayerStateFactory {
-    constructor () {
-        this.groundedState = new PlayerGroundedState();
-        this.jumpingState = new PlayerJumpingState();
-        this.fallingState = new PlayerFallingState();
-    }
-}
